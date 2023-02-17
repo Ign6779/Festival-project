c984bdeb8a00e4aec588de207843935774c3a4d4
Salah-023 <90417315+Salah-023@users.noreply.github.com>
2023-02-17 17:00:43 +0100

    Update homepage.php
    i included the header in the homepage so the styling is applied

--- app/views/home/homepage.php
+++ app/views/home/homepage.php
@@ -1,3 +1,5 @@
+<? include __DIR__ . '/../header.php' ?>
+
 <!-- font families  -->
 <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Corben" />
@@ -5,84 +7,92 @@
 
 
 
-<ol>
+<!-- <ol>
     <li><a href="festival/overview">GOTO overview</a></li>
     <li><a href="festival/dance">GOTO dance</a></li>
     <li><a href="festival/history">GOTO history</a></li>
     <li><a href="festival/jazz">GOTO jazz</a></li>
     <li><a href="festival/kids">GOTO kids</a></li>
     <li><a href="festival/yummy">GOTO yummy</a></li>
-</ol>
+</ol> -->
 
 <body>
-<!-- Header of the homepage -->
-<section class="homepage-header">
-<h1>Welcome to Haarlem</h1>
-<h3>Welcome to the city of endless possibilities: A charming historic district, fascinating museums, unique shops, a wide variety of dining options, and a lovely beach, 
-<br></h3>
-<h2>Haarlem really has it all.</h2>
-<br><a class="btn-view-more" href="festival/overview">View Festival Activities</a>
-</section>
+    <!-- Header of the homepage -->
+    <section class="homepage-header">
+        <h1>Welcome to Haarlem</h1>
+        <h3>Welcome to the city of endless possibilities: A charming historic district, fascinating museums, unique
+            shops, a wide variety of dining options, and a lovely beach,
+            <br>
+        </h3>
+        <h2>Haarlem really has it all.</h2>
+        <br><a class="btn-view-more" href="festival/overview">View Festival Activities</a>
+    </section>
 
-<div class="homepage-history container-fluid">
-    <div>
-    <h2>Haarlem's History</h2>
+    <div class="homepage-history container-fluid">
+        <div>
+            <h2>Haarlem's History</h2>
+        </div>
+        <div>
+            <h3>Haarlem has been an important Dutch city since 1245, functioning initially as a shipping sea port.
+                The majority of Haarlem's old gorgeous buildings have stayed intact and are what make the atmosphere in
+                Haarlem truly amazing. Well known for its beer making, Haarlem is home to the wonderful Jopenkerk and
+                windmill de Adriaan from 1779.</h3>
+        </div>
     </div>
-    <div>
-    <h3>Haarlem has been an important Dutch city since 1245, functioning initially as a shipping sea port. 
-    The majority of Haarlem's old gorgeous buildings have stayed intact and are what make the atmosphere in Haarlem truly amazing. Well known for its beer making, Haarlem is home to the wonderful Jopenkerk and windmill de Adriaan from 1779.</h3>
+
+    <section class="homepage-food">
+        <div>
+            <h3>No matter when, dining in Haarlem is guaranteed to be an incredible experience.
+                Not only does Haarlem have some of the best restaurants with quality food, the atmosphere of the city is
+                what makes it all the more special. </h3>
+        </div>
+        <div>
+            <h2>For Foodies!</h2>
+        </div>
+        <div>
+        </div>
+    </section>
+    <div class="grid">
+        <section class="homepage-music">
+            <h2>Music</h2>
+            <h3>The city of Haarlem comes alive with music in July. The days are longer and warmer, making it the ideal
+                time to go see both well-known and unknown Dutch musicians play for the delight of the crowd.</h3>
+        </section>
+
+        <section class="homepage-theatre">
+            <h2>Theatre</h2>
+            <h3>When it comes to entertainment, Haarlem is perfect. Theatres such as :Het Kennenmer Theatre, and
+                Philharmonie will guarantee you won't leave Haarlem disappointed.
+                Don't miss out on the beautiful dramas and the high quality productions!</h3>
+        </section>
+
+        <section class="homepage-museums">
+            <h2>Museums</h2>
+            <h3>If you want to see some amazing historical artefacts and you want to learn their history, then come to
+                Haarlem’s museums!<br>Some great choices include:
+                <br>&#x2022;The corrie ten boom house
+                <br>&#x2022;Teylers Museum
+                <br>&#x2022;Frans hals museum
+                <br>&#x2022;Museum van de geest
+            </h3>
+        </section>
     </div>
-</div>
-
-<section class="homepage-food">
-    <div>
-    <h3>No matter when, dining in Haarlem is guaranteed to be an incredible experience.
-Not only does Haarlem have some of the best restaurants with quality food, the atmosphere of the city is what makes it all the more special. </h3>
-    </div>
-    <div>
-        <h2>For Foodies!</h2>
-    </div>
-    <div>
-    </div>
-</section>
-<div class = "grid">
-<section class="homepage-music">
-<h2>Music</h2>
-    <h3>The city of Haarlem comes alive with music in July. The days are longer and warmer, making it the ideal time to go see both well-known and unknown Dutch musicians play for the delight of the crowd.</h3>
-</section>
-
-<section class="homepage-theatre">
-    <h2>Theatre</h2>
-    <h3>When it comes to entertainment, Haarlem is perfect. Theatres such as :Het Kennenmer Theatre, and Philharmonie will guarantee you won't leave Haarlem disappointed. 
-Don't miss out on the beautiful dramas and the high quality productions!</h3>
-</section>
-
-<section class="homepage-museums">
-    <h2>Museums</h2>
-    <h3>If you want to see some amazing historical artefacts and you want to learn their history, then come to Haarlem’s museums!<br>Some great choices include:
-<br>&#x2022;The corrie ten boom house
-<br>&#x2022;Teylers Museum
-<br>&#x2022;Frans hals museum
-<br>&#x2022;Museum van de geest</h3>
-</section>
-</div>
 
 
-<section class="homepage-festival">
-    <h2>Check out the festival here!</h2>
-    <a class="festival-button" href="festival/overview">See festival events</a>
-</section>
+    <section class="homepage-festival">
+        <h2>Check out the festival here!</h2>
+        <a class="festival-button" href="festival/overview">See festival events</a>
+    </section>
 
-<section class="homepage-mobile">
-    <h2>Visit us on mobile.</h2>
-    <h3> Scan the QR code with your phone.</h3>
-</section>
+    <section class="homepage-mobile">
+        <h2>Visit us on mobile.</h2>
+        <h3> Scan the QR code with your phone.</h3>
+    </section>
 
 
 
 
 </body>
 <?php
-include  __DIR__ . '/../footer.php';
+include __DIR__ . '/../footer.php';
 ?>
-
