a0fa00113b5a14249f4ea33570cde5f1f9c0fe5f
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2023-02-17 11:40:36 +0100

    user repo getAll and getByUsername

--- app/repositories/userrepository.php
+++ app/repositories/userrepository.php
@@ -1,4 +1,42 @@
 <?php
 require __DIR__ . '/repository.php';
-require __DIR__ . '/../models/'
+require __DIR__ . '/../models/user.php';
+
+class UserRepository extends Repository {
+    function getAll() {
+        try {
+            $stmt = $this->connection->prepare("SELECT * FROM users");
+            $stmt->execute();
+
+            $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
+            $users = $stmt->fetchAll();
+
+            return $users;
+        }
+        catch (PDOException $e){
+            echo $e;
+        }
+    }
+
+    function getByUsername($username) {
+        try {
+            $stmt = $this->connection->prepare("SELECT * FROM users WHERE username = ?");
+            $stmt->execute($username);
+
+            $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
+            $user = $stmt->fetch();
+
+            return $user;
+        }
+        catch (PDOException $e){
+            echo $e;
+        }
+    }
+
+    // function insert($user) {
+    //     try {
+    //         $stmt = $this->connection->prepare("INSERT INTO users ()")
+    //     }
+    // }
+}
 ?>
