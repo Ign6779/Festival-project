eb7f598eef9120331504ce1e9d21978977007e1b
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2023-02-23 16:34:27 +0100

    changes in erd make jazz changes obsolete

--- app/services/userservice.php
+++ app/services/userservice.php
@@ -2,7 +2,7 @@
 require __DIR__ . '/../repositories/userrepository.php';
 
 class UserService {
-    public function getAll(){
+    public function getAll() {
         $repository = new UserRepository();//might declare this on the UserService constructor, so there is no duplicate
         $users = $repository->getAll();
         return $users;
