627556ba41a985a447c6dc727c2dd588f330a441
myrthe678105 <90244561+myrthe678105@users.noreply.github.com>
2023-03-01 11:01:39 +0100

    insta feed is functional

--- app/views/home/homepage.php
+++ app/views/home/homepage.php
@@ -87,6 +87,55 @@
     <h2>Visit us on mobile.</h2>
     <h3> Scan the QR code with your phone.</h3>
 </section>
+<!-- instagram feed -->
+<?php 
+// query the user media
+$fields = "id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username";
+$token = "IGQVJWX3NNQmw3elNsdlhnZA21yMTRvSjNxaHd6eXJWOEpBbEQzNDZAyb1ZAIbUJ3V2dVcUFPTkRoZAmxyY3dEQ2g5NHVfcS1xYTFzSk1COTduY2Uxa1ZA4RHgyM3FGdG5RMUNsODFvcGRUT3UyRy1TTklkVAZDZD";
+$limit = 10;
+
+$json_feed_url="https://graph.instagram.com/me/media?fields={$fields}&access_token={$token}&limit={$limit}";
+$json_feed = @file_get_contents($json_feed_url);
+$contents = json_decode($json_feed, true, 512, JSON_BIGINT_AS_STRING);
+
+echo "<div class='ig_feed_container'>";
+    foreach($contents["data"] as $post){
+        
+        $username = isset($post["username"]) ? $post["username"] : "";
+        $caption = isset($post["caption"]) ? $post["caption"] : "";
+        $media_url = isset($post["media_url"]) ? $post["media_url"] : "";
+        $permalink = isset($post["permalink"]) ? $post["permalink"] : "";
+        $media_type = isset($post["media_type"]) ? $post["media_type"] : "";
+        
+        echo "
+            <div class='ig_post_container'>
+                <div>";
+
+                    if($media_type=="VIDEO"){
+                        echo "<video controls style='width:100%; display: block !important;'>
+                            <source src='{$media_url}' type='video/mp4'>
+                            Your browser does not support the video tag.
+                        </video>";
+                    }
+
+                    else{
+                        echo "<img src='{$media_url}' />";
+                    }
+                
+                echo "</div>
+                <div class='ig_post_details'>
+                    <div>
+                        <strong>@{$username}</strong> {$caption}
+                    </div>
+                    <div class='ig_view_link'>
+                        <a href='{$permalink}' target='_blank'>View on Instagram</a>
+                    </div>
+                </div>
+            </div>
+        ";
+    }
+echo "</div>"
+?>
 
 
 
