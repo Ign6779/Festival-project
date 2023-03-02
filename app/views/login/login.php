5a588ac6971db65ca7630e148b6187542b679419
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-03-02 14:52:13 +0100

    google api

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -58,7 +58,7 @@ include __DIR__ . '/../header.php';
           <div class="text-center text-lg-start mt-4 pt-2">
             <button type="button" class="btn btn-primary btn-lg"
               style="padding-left: 2.5rem; padding-right: 2.5rem;" onclick="redirct('/')">Login</button>
-            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
+            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="/login/register"
                 class="link-danger">Register</a></p>
           </div>
 
@@ -77,3 +77,42 @@ include __DIR__ . '/../header.php';
 <?php
 include __DIR__ . '/../footer.php';
 ?>
+
+<?php //require ("vendor/autoload.php");
+//Step 1: Enter you google account credentials
+
+$g_client = new Google_Client();
+
+$g_client->setClientId("635746243599-n4uqgio77g1lgbkgkmjajoogd49o7e7k.apps.googleusercontent.com");
+$g_client->setClientSecret("Hgq0sN65ue-0E4dyGsS0WZlJ");
+$g_client->setRedirectUri("http://localhost/");
+$g_client->setScopes("email");
+
+//Step 2 : Create the url
+$auth_url = $g_client->createAuthUrl();
+echo "<a href='$auth_url'>Login Through Google </a>";
+
+//Step 3 : Get the authorization  code
+$code = isset($_GET['code']) ? $_GET['code'] : NULL;
+
+//Step 4: Get access token
+if(isset($code)) {
+    try {
+        $token = $g_client->fetchAccessTokenWithAuthCode($code);
+        $g_client->setAccessToken($token);
+
+    }catch (Exception $e){
+        echo $e->getMessage();
+    }
+    try {
+        $pay_load = $g_client->verifyIdToken();
+    }catch (Exception $e) {
+        echo $e->getMessage();
+    }
+} else{
+    $pay_load = null;
+}
+
+if(isset($pay_load)){
+
+}?>
---
+++ app/views/login/register.php
@@ -0,0 +1,94 @@
+<?php
+include __DIR__ . '/../header.php';
+?>
+
+<section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="/img/login.png"
+          class="img-fluid" alt="Sample image">
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <form>
+          <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+            <p class="lead fw-normal mb-0 me-3">Sign in with</p>
+            <button type="button" class="btn btn-primary btn-floating mx-1">
+              <i class="fab fa-facebook-f"></i>
+            </button>
+
+            <button type="button" class="btn btn-primary btn-floating mx-1">
+              <i class="fab fa-twitter"></i>
+            </button>
+
+            <button type="button" class="btn btn-primary btn-floating mx-1">
+              <i class="fab fa-google"></i>
+            </button>
+          </div>
+
+          <div class="divider d-flex align-items-center my-4">
+            <p class="text-center fw-bold mx-3 mb-0">Or</p>
+          </div>
+
+          <!-- Name input -->
+          <div class="form-outline mb-4">
+            <input type="text" id="form3Example3" class="form-control form-control-lg"
+              placeholder="Enter your username" />
+            <label class="form-label" for="form3Example3">Username</label>
+          </div>
+
+          <!-- Email input -->
+          <div class="form-outline mb-4">
+            <input type="email" id="form3Example3" class="form-control form-control-lg"
+              placeholder="Enter a valid email address" />
+            <label class="form-label" for="form3Example3">Email address</label>
+          </div>
+
+          <!-- Password input -->
+          <div class="form-outline mb-3">
+            <input type="password" id="form3Example4" class="form-control form-control-lg"
+              placeholder="Password" />
+            <label class="form-label" for="form3Example4">Password</label>
+          </div>
+
+          <!-- Repeat Password input -->
+          <div class="form-outline mb-3">
+            <input type="password" id="form3Example4" class="form-control form-control-lg"
+              placeholder="Repeat Password" />
+            <label class="form-label" for="form3Example4">Password</label>
+          </div>
+
+          <div class="form-check mb-0">
+            <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+            <label class="form-check-label" for="form2Example3">
+                I agree all statements in <a href="#!">Terms of service</a>
+            </label>
+          </div>
+
+            <div class="row">
+                <div class="col-sm-3 text-center text-lg-start mt-4 pt-2">
+                    <button type="button" class="btn btn-primary btn-lg"
+                    style="padding-left: 2.5rem; padding-right: 2.5rem;" onclick="redirct('/')">Register</button>
+                </div>
+                <!-- Button for goin back to the login page
+                <div class="col-sm-3 text-center text-lg-start mt-4 pt-2">
+                    <button type="button" class="btn btn-primary btn-lg"
+                    style="padding-left: 2.5rem; padding-right: 2.5rem;" onclick="redirct('/login/login')">Back</button>
+                </div>
+                -->
+            </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+<script>
+        function redirct(src) {
+            window.location = src;
+        }
+</script>
+
+<?php
+include __DIR__ . '/../footer.php';
+?>
