52a75f1cf3cbc923f7773cef47a232e57fdfa786
myrthe678105 <90244561+myrthe678105@users.noreply.github.com>
2023-03-07 10:25:44 +0100

    own css file

--- app/views/header.php
+++ app/views/header.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="/css/history.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="/css/login.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="/css/dance.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="/css/overview.css?v=<?php echo time(); ?>">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
