f83d93ed1897546061300b878a0f99c1dc4b92b9
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2023-03-08 23:10:47 +0100

    commit because network adapter failed again

--- app/views/festival/yummy.php
+++ app/views/festival/yummy.php
@@ -2,11 +2,29 @@
 include __DIR__ . '/../header.php';
 ?>
 <head>
-  <link rel="stylesheet" href="/../../public/css/yummy.css">
+  <link rel="stylesheet" href="/../css/yummy.css">
+  <link rel="stylesheet" href="/../css/components.css">
 </head>
-<div class="yummy-header">
-  <h1 class="yummy-title">YUMMY! HAARLEM</h1>
-</div>
+<body>
+  <div id="yummy-header">
+    <h1 id="yummy-title">YUMMY! HAARLEM</h1>
+    <button class="red-button" id="header-button">Buy tickets</button>
+  </div>
+
+  <div id="yummy-introduction">
+    <p><b>What is a festival without food?</b> The restaurants here in Haarlem have decided to create special menus to celebrate the festival! Enjoy a fun family dining experience, or a romantic date with your partner with our prepared catering at the restaurants listed below. Each restaurant has a 1.5-2h tasting session prepared, where you may try classical Dutch or French high cuisine, Asian or even a mix of all!</p>
+  </div>
+
+  <section id="restaurant-info">
+    <div id="restaurant-cards">
+      these are the cards
+    </div>
+
+    <aside id="map-aside">
+      this will be the map
+    </aside>
+  </section>
+  
 <?php
 include __DIR__ . '/../footer.php';
 ?>
