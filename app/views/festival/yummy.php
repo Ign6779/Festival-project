06cbeca0ce60c6693cb9e08cbf8559e0fd42fdd5
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2023-03-08 21:09:46 +0100

    ugh

--- app/views/festival/yummy.php
+++ app/views/festival/yummy.php
@@ -1,6 +1,9 @@
 <?php
 include __DIR__ . '/../header.php';
 ?>
+<head>
+  <link rel="stylesheet" href="/../../public/css/yummy.css">
+</head>
 <div class="yummy-header">
   <h1 class="yummy-title">YUMMY! HAARLEM</h1>
 </div>
