a748578918d013ff38af42d8badbe027d0dd08ac
Salah-023 <90417315+Salah-023@users.noreply.github.com>
2023-03-09 15:59:19 +0100

    fixed the bug

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -9,7 +9,6 @@ include __DIR__ . '/../header.php';
         <img src="/img/login.png" class="img-fluid" alt="Sample image">
       </div>
       <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-        <!-- action="/login/login" -->
         <form id="loginform" method="post">
           <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
             <p class="lead fw-normal mb-0 me-3">Sign in with</p>
@@ -17,36 +16,38 @@ include __DIR__ . '/../header.php';
             <button type="button" class="btn btn-primary btn-floating mx-1">
               <i class="fab fa-google"></i>
             </button>
+          </div>
 
-            <!-- Email input -->
-            <div class="form-outline mb-4">
-              <input type="text" id="form3Example4" name="emailInput" class="form-control form-control-lg"
-                placeholder="Enter a valid email address" />
-              <label class="form-label" for="form3Example3">Email address</label>
-            </div>
+          <div class="divider d-flex align-items-center my-4">
+            <p class="text-center fw-bold mx-3 mb-0">Or</p>
+          </div>
 
-            <!-- Password input -->
-            <div class="form-outline mb-3">
-              <input type="password" id="form3Example4" name="passwordInput" class="form-control form-control-lg"
-                placeholder="Enter password" />
-              <label class="form-label" for="form3Example4">Password</label>
-            </div>
+          <!-- Email input -->
+          <div class="form-outline mb-4">
+            <input type="text" id="form3Example4" name="emailInput" class="form-control form-control-lg"
+              placeholder="Enter a valid email address" />
+            <label class="form-label" for="form3Example3">Email address</label>
+          </div>
 
-            <div class="d-flex justify-content-between align-items-center">
-              <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="/login/register"
-                  class="link-danger">Register</a></p>
-              <a href="/login/forgotpassword" class="text-body">Forgot password?</a>
-            </div>
+          <!-- Password input -->
+          <div class="form-outline mb-3">
+            <input type="password" id="form3Example4" name="passwordInput" class="form-control form-control-lg"
+              placeholder="Enter password" />
+            <label class="form-label" for="form3Example4">Password</label>
+          </div>
 
-            <div class="g-recaptcha" data-sitekey="6LeOjOgkAAAAAMCoe4KB9sd146Bkg3DEabH1cFVt"></div>
+          <div class="g-recaptcha" data-sitekey="6LeOjOgkAAAAAMCoe4KB9sd146Bkg3DEabH1cFVt"></div>
 
-            <div class="text-center text-lg-start mt-4 pt-2">
-             
-                <input type="submit" class="btn btn-primary btn-lg" id="login" name="login"
-                  style="padding-left: 2.5rem; padding-right: 2.5rem;">
-                <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="/login/register"
-                    class="link-danger">Register</a></p>
-            </div>
+          <div class="d-flex justify-content-between align-items-center">
+            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="/login/register"
+                class="link-danger">Register</a></p>
+            <a href="/login/forgotpassword" class="text-body">Forgot password?</a>
+          </div>
+
+          <div class="text-center text-lg-start mt-4 pt-2">
+            <input type="submit" class="btn btn-primary btn-lg" id="login"
+              style="padding-left: 2.5rem; padding-right: 2.5rem;" name="login" value="Login">
+          </div>
 
         </form>
       </div>
@@ -54,25 +55,24 @@ include __DIR__ . '/../header.php';
   </div>
 </section>
 
-
-  <script>
-    $(document).on('click', '#login', function () {
-      var response = grecaptcha.getResponse();
-      if (response.length == 0) {
-        alert("Please verify you are not robot");
-        return false;
-      }
-      else {
-        document.getElementById('loginform').action;
-        document.getElementById('loginform').action = "/login/login";
-      }
-    });
-
-    function redirect() {
-      window.location = '/';
+<script>
+  $(document).on('click', '#login', function () {
+    var response = grecaptcha.getResponse();
+    if (response.length == 0) {
+      alert("Please verify you are not robot");
+      return false;
     }
-  </script>
+    else {
+      document.getElementById('loginform').action;
+      document.getElementById('loginform').action = "/login/login";
+    }
+  });
 
-  <?php
-  include __DIR__ . '/../footer.php';
-  ?>
+  function redirect() {
+    window.location = '/';
+  }
+</script>
+
+<?php
+include __DIR__ . '/../footer.php';
+?>
