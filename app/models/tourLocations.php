d7b31118216ebcf6fcc2f8c5f1c114cb6a0e8db1
aleksandrabivolarska <678906@student.inholland.nl>
2023-03-17 12:40:56 +0100

    tour repo and model

---
+++ app/models/tourLocations.php
@@ -0,0 +1,67 @@
+<?php
+
+class TourLocations implements \JsonSerializable {
+    private int $id;
+
+    private string $name;
+
+    private string $location;
+
+    private string $description;
+
+    private string $picture;
+
+    #[ReturnTypeWillChange]
+
+    public function jsonSerialize() {
+        $vars = get_object_vars($this);
+        return $vars;
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function setId($id): self {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function setName($name): self {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getLocation(): string {
+        return $this->location;
+    }
+
+    public function setLocation($location): self {
+        $this->location = $location;
+        return $this;
+    }
+
+    public function getDescription(): string {
+        return $this->description;
+    }
+
+    public function setDescription($description): self {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function setPicture(): string {
+        return $this->picture;
+    }
+
+    public function getPicture($picture): self {
+        $this->picture = $picture;
+        return $this;
+    }
+}
+
+?>
---
+++ app/repositories/tourlocationsrepository.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__ . '/repository.php';
+require __DIR__ . '/../models/tourLocations.php';
+
+?>
