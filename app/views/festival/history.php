e86705cf4d39e03b0dd2575df0dbe0ce03a52652
aleksandrabivolarska <678906@student.inholland.nl>
2023-03-21 10:13:17 +0100

    carousel css

--- app/views/festival/history.php
+++ app/views/festival/history.php
@@ -19,29 +19,13 @@ function returnActive($count)
 </div>
 
 <section class="history-timetable">
-    <h2 id="opit">Timetable</h2>
-    <!-- <table>
-        <tr class="history-timetable-header">
-            <th class="history timetable">Date</th>
-            <th class="history timetable">Hour</th>
-            <th class="history timetable">Available seats<br>English tour</th>
-            <th class="history timetable">Available seats<br>Dutch tour</th>
-            <th class="history timetable">Available seats<br>Chinese tour</th>
-        </tr>
-        <tr>
-
-        </tr>
-
-        <tr>
-        </tr>
-    </table> -->
+    <h2>Timetable</h2>
     <?php
     echo '<table>';
-    echo '<thead><tr><th>id</th><th>Date</th><th>Time</th><th>Available seats English</th><th>Available seats Nl</th><th>Available seats Ch</th></thead>';
+    echo '<thead><tr><th>Date</th><th>Time</th><th>Available seats<br>English</th><th>Available seats<br>Dutch</th><th>Available seats<br>Chinese</th></thead>';
     echo '<tbody>';
     foreach ($tours as $tour) {
         echo '<tr>';
-        echo '<td>' . $tour->getId() . '</td>';
         echo '<td>' . $tour->getDate() . '</td>';
         echo '<td>' . $tour->getTime() . '</td>';
         echo '<td>' . $tour->getAvaliableSeatsEn() . '</td>';
@@ -65,7 +49,7 @@ function returnActive($count)
                 <div id="<? echo $location->getId(); ?>" class="carousel-item <? echo returnActive($count);
                    $count++; ?>">
                     <div class="d-flex justify-content-center w-100 h-100">
-                        <img src="/img/<? echo $location->getImage(); ?>" class="align-middle w-50 "
+                        <img src="/img/<? echo $location->getImage(); ?>" class="align-middle w-30 " id="carousel-history-image"
                             alt="<? echo $location->getImage(); ?>">
                     </div>
                 </div>
@@ -75,12 +59,12 @@ function returnActive($count)
 
         <button class="carousel-control-prev " type="button" data-bs-target="#carouselExampleControls"
             data-bs-slide="prev">
-            <span class="carousel-control-prev-icon btn btn-primary" aria-hidden="true"></span>
+            <span class="carousel-control-prev-icon btn btn-primary" id ="tour-carousel-prev"aria-hidden="true"><p class="history-carousel-arrows">&#8592 </p></span>
             <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next " type="button" data-bs-target="#carouselExampleControls"
             data-bs-slide="next">
-            <span class="carousel-control-next-icon btn btn-primary" aria-hidden="true"></span>
+            <span class="carousel-control-next-icon btn btn-primary" id="tour-carousel-next" aria-hidden="true"><p class="history-carousel-arrows">&#8594 </p></span>
             <span class="visually-hidden">Next</span>
         </button>
     </div>
@@ -96,45 +80,13 @@ function returnActive($count)
     <img src="/img/history-map.png" alt="map of tour locations" class="history-tour-map" />
     <div class="tour-locations">
         <h2>Tour Locations</h2>
-        <ul class="tour-locations-list">
-            <li>
-                <div class="li-circle">1</div>
-                <div class="li-options">Start location:<br>Bavo Church (Grote Markt 22, 2011 RD Haarlem)</div>
-            </li>
-            <li>
-                <div class="li-circle">2</div>
-                <div class="li-options">Grote Markt (Grote Markt, 2011 RD Haarlem) </div>
-            </li>
-            <li>
-                <div class="li-circle">3</div>
-                <div class="li-options">De Hallen Haarlem (Grote Markt 16, 2011 RD Haarlem)</div>
-            </li>
-            <li>
-                <div class="li-circle">4</div>
-                <div class="li-options">Proveniershof (Grote Houtstraat 142D, 2011 SV Haarlem) </div>
-            </li>
-            <h3>Break location:</h3>
-            <li>
-                <div class="li-circle">5</div>
-                <div class="li-options">Jopenkerk (Gedempte Voldersgracht 2, 2011 WD Haarlem) </div>
-            </li>
-            <li>
-                <div class="li-circle">6</div>
-                <div class="li-options">Waalse Kerk (Begijnhof 28, 2011 HE Haarlem) </div>
-            </li>
-            <li>
-                <div class="li-circle">7</div>
-                <div class="li-options">Molen de Adriaan (Papentorenvest 1A, 2011 AV Haarlem) </div>
-            </li>
-            <li>
-                <div class="li-circle">8</div>
-                <div class="li-options">Amsterdamse Poort (2011 BZ Haarlem) </div>
-            </li>
-            <li>
-                <div class="li-circle">9</div>
-                <div class="li-options">Hofje van Bakenes (Wijde Appelaarsteeg 11F, 2011HB Haarlem) </div>
-            </li>
-        </ul>
+        <ol class="tour-locations-li">
+            <?php
+            foreach ($locations as $location) {
+                echo '<li>'.$location->getName().'('.$location->getLocation().')'.'</li>';
+            }
+            ?>
+        </ol>
     </div>
 </section>
 
@@ -164,11 +116,11 @@ function returnActive($count)
         header.innerHTML = locationInput.name;
         pargraph = document.createElement("p");
         pargraph.innerHTML = locationInput.description;
-        img = document.createElement("img");
-        img.src = "/img/" + locationInput.img;
+        // img = document.createElement("img");
+        // img.src = "/img/" + locationInput.img;
         location.appendChild(header);
         location.appendChild(pargraph);
-        location.appendChild(img);
+        // location.appendChild(img);
     }
 
 </script>
