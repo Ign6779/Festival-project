52736ea8b349fa4fc4471d480490f5355bab1e23
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-03-27 14:04:06 +0200

    session start

--- app/public/index.php
+++ app/public/index.php
@@ -4,6 +4,8 @@ require __DIR__ . '/../patternrouter.php';
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 
+session_start();
+
 $uri = trim($_SERVER['REQUEST_URI'], '/');
 
 $router = new PatternRouter();
