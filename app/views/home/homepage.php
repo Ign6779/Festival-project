b4abfd01c31a27e36ab37c1ecbb00362c1ece69b
Salah-023 <90417315+Salah-023@users.noreply.github.com>
2023-03-30 14:01:51 +0200

    LATEST CHANGES

--- app/views/home/homepage.php
+++ app/views/home/homepage.php
@@ -3,30 +3,30 @@
 <!-- Header of the homepage -->
 <section class="homepage-header">
     <div class="homepage-picture">
-    <img src="/img/homepage-main.png" alt="Haarlem-overview" class="img-responsive">
-    <h1 id="homepage-welcome">Welcome to Haarlem</h1>
-</div>
+        <img src="/img/homepage-main.png" alt="Haarlem-overview" class="img-responsive">
+        <h1 id="homepage-welcome">Welcome to Haarlem</h1>
+    </div>
 </section>
 <section class="homepage-context">
 
-<h3>Welcome to the city of endless possibilities: A charming historic district, fascinating museums, unique <br>
-            shops, a wide variety of dining options, and a lovely beach,
-            <br>
-        </h3>
-        <h2>Haarlem really has it all.</h2>
-        <br><a class="btn-red" href="festival">View Festival Activities</a>
-    </section>
+    <h3>Welcome to the city of endless possibilities: A charming historic district, fascinating museums, unique <br>
+        shops, a wide variety of dining options, and a lovely beach,
+        <br>
+    </h3>
+    <h2>Haarlem really has it all.</h2>
+    <br><a class="btn-red" href="festival">View Festival Activities</a>
+</section>
 
 <!-- History section -->
 <div class="homepage-history">
     <div>
         <h2>Haarlem's History</h2>
-        <img id = "homepage-history-1" src="/img/homepage-history-1.png"/>
+        <img id="homepage-history-1" src="/img/homepage-history-1.png" />
     </div>
     <div>
-        <img id="homepage-history-2" src="/img/homepage-history-2.png"/>
-        <img id="homepage-history-3"src="/img/homepage-history-3.png"/>
-        <img id="homepage-history-4"src="/img/homepage-history-4.png"/>
+        <img id="homepage-history-2" src="/img/homepage-history-2.png" />
+        <img id="homepage-history-3" src="/img/homepage-history-3.png" />
+        <img id="homepage-history-4" src="/img/homepage-history-4.png" />
     </div>
     <div id="homepage-history-text">
         <h3>Haarlem has been an important Dutch city since 1245, functioning initially as a shipping sea port.
@@ -46,10 +46,10 @@
     </div>
     <div id="homepage-food-mid-section">
         <h2 id="for-foodies-text">For Foodies!</h2>
-        <img src="/img/food-icon.png" id="food-icon" alt="food and drink"/>
+        <img src="/img/food-icon.png" id="food-icon" alt="food and drink" />
     </div>
     <div>
-        
+
     </div>
 </section>
 
@@ -62,28 +62,28 @@
             time to go see both well-known and unknown Dutch musicians play for the delightz of the crowd.</h3>
     </section>
 
-        <!-- Theatre section -->
-        <section class="homepage-theatre">
-            <h2>Theatre</h2>
-            <img src="/img/homepage-theatre.png" alt="Theatre" width=95%>
-            <h3>When it comes to entertainment, Haarlem is perfect. Theatres such as: Het Kennenmer Theatre, and
-                Philharmonie will guarantee you won't leave Haarlem disappointed.
-                Don't miss out on the beautiful dramas and the high quality productions!</h3>
-        </section>
+    <!-- Theatre section -->
+    <section class="homepage-theatre">
+        <h2>Theatre</h2>
+        <img src="/img/homepage-theatre.png" alt="Theatre" width=95%>
+        <h3>When it comes to entertainment, Haarlem is perfect. Theatres such as: Het Kennenmer Theatre, and
+            Philharmonie will guarantee you won't leave Haarlem disappointed.
+            Don't miss out on the beautiful dramas and the high quality productions!</h3>
+    </section>
 
-        <!-- Museums section -->
-        <section class="homepage-museums">
-            <h2>Museums</h2>
-            <img src="/img/homepage-museums.png" alt="Museums" width=95%>
-            <h3>If you want to see some amazing historical artefacts and you want to learn their history, then come to
-                Haarlem's museums!<br>Some great choices include:
-                <br>&#x2022;The corrie ten boom house
-                <br>&#x2022;Teylers Museum
-                <br>&#x2022;Frans hals museum
-                <br>&#x2022;Museum van de geest
-            </h3>
-        </section>
-    </div>
+    <!-- Museums section -->
+    <section class="homepage-museums">
+        <h2>Museums</h2>
+        <img src="/img/homepage-museums.png" alt="Museums" width=95%>
+        <h3>If you want to see some amazing historical artefacts and you want to learn their history, then come to
+            Haarlem's museums!<br>Some great choices include:
+            <br>&#x2022;The corrie ten boom house
+            <br>&#x2022;Teylers Museum
+            <br>&#x2022;Frans hals museum
+            <br>&#x2022;Museum van de geest
+        </h3>
+    </section>
+</div>
 
 <!-- Festival section -->
 <section class="homepage-festival">
@@ -100,42 +100,40 @@
 <div class="homepage-instagram">
     <h2 class="ig-title">Keep up with our instragram below!</h2>
 </div>
-<?php 
+<?php
 // query the user media
 
 $fields = "id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username";
-$token = "IGQVJYeVEwb2lNLTdxNVc1OFYwQVNVazg3aFV6eXZAfWGZALOEJGUTJzWjYyLWxWaGljWnNXWVRxX3FJY0RSUU5kZA2VaVVhEbHpKZAVY5a2JBRjZAYVzFKV0tyTzZAoLUpCWmY0bW9QQkhqRWVrcEJCaVNxWQZDZD";
+$token = "IGQVJVdDBjRlNMZAVNtWm9TUDdKY2VjWTlPczB3b21DU2h0MUhwUFlUc1ctNWRqNVFrbjZA1OXpNTXRONVRJREVZAMk5Ua0lDZAHVIWDh4SjZAsOGdqQlEtWHZAnTnQ5WDktVVNjT3gwRGdEZAExqU0dZAQmtWUQZDZD";
 $limit = 10;
 
-$json_feed_url="https://graph.instagram.com/me/media?fields={$fields}&access_token={$token}&limit={$limit}";
+$json_feed_url = "https://graph.instagram.com/me/media?fields={$fields}&access_token={$token}&limit={$limit}";
 $json_feed = @file_get_contents($json_feed_url);
 $contents = json_decode($json_feed, true, 512, JSON_BIGINT_AS_STRING);
 
 echo "<div class='ig_feed_container'>";
-    foreach($contents["data"] as $post){
-        
-        $username = isset($post["username"]) ? $post["username"] : "";
-        $caption = isset($post["caption"]) ? $post["caption"] : "";
-        $media_url = isset($post["media_url"]) ? $post["media_url"] : "";
-        $permalink = isset($post["permalink"]) ? $post["permalink"] : "";
-        $media_type = isset($post["media_type"]) ? $post["media_type"] : "";
-        
-        echo "
+foreach ($contents["data"] as $post) {
+
+    $username = isset($post["username"]) ? $post["username"] : "";
+    $caption = isset($post["caption"]) ? $post["caption"] : "";
+    $media_url = isset($post["media_url"]) ? $post["media_url"] : "";
+    $permalink = isset($post["permalink"]) ? $post["permalink"] : "";
+    $media_type = isset($post["media_type"]) ? $post["media_type"] : "";
+
+    echo "
             <div class='ig_post_container'>
                 <div>";
 
-                    if($media_type=="VIDEO"){
-                        echo "<video controls style='width:100%; display: block !important;'>
+    if ($media_type == "VIDEO") {
+        echo "<video controls style='width:100%; display: block !important;'>
                             <source src='{$media_url}' type='video/mp4'>
                             Your browser does not support the video tag.
                         </video>";
-                    }
+    } else {
+        echo "<img src='{$media_url}' />";
+    }
 
-                    else{
-                        echo "<img src='{$media_url}' />";
-                    }
-                
-                echo "</div>
+    echo "</div>
                 <div class='ig_post_details'>
                     <div>
                         <strong>@{$username}</strong> {$caption}
@@ -146,13 +144,13 @@ echo "<div class='ig_feed_container'>";
                 </div>
             </div>
         ";
-    }
+}
 echo "</div>"
-?>
+    ?>
 
 <!-- pointer to the top of the page -->
 <div class="pointer-to-top">
-<a class="arrow-up" href="#top">&#8593</a>
+    <a class="arrow-up" href="#top">&#8593</a>
 </div>
 
 
